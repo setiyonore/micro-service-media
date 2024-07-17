ed5495e12b9e50c8dd9db317991473ba846efd12
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-07-17 20:18:28 +0700

    add delete image

--- routes/media.js
+++ routes/media.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 const { Media } = require('../models');
+const fs = require('fs');
 router.get('/', async (req, res) => {
   const media = await Media.findAll({
     attributes: ['id', 'image'],
@@ -43,6 +44,29 @@ router.post('/', (req, res) => {
 
 
   })
+});
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  const media = await Media.findByPk(id);
+  if (!media) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'media not found'
+    });
+  }
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({
+        status: 'error',
+        message: err.message
+      });
+    }
+    await media.destroy();
+    return res.status(200).json({
+      status: 'success',
+      message: 'image deleted',
+    });
+  });
 });
 
 module.exports = router;
